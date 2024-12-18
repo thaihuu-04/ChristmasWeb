c80593f2005b309418649c1b0eacb99e3fd13e2d
thaihuu-04 <thaihuu0409@gmail.com>
2024-12-18 22:23:45 +0700

    hotfixV1 rl 18-12-2024 huu

--- gift/script.js
+++ gift/script.js
@@ -159,32 +159,6 @@ function createStars() {
   setInterval(createGift, 1000); // 8 giây một lần
   
   
-  function updateCountdown() {
-    const christmas = new Date(new Date().getFullYear(), 11, 25);
-    const now = new Date();
-    const diff = christmas - now;
-  
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-  
-    document.getElementById('days').textContent = days
-      .toString()
-      .padStart(2, '0');
-    document.getElementById('hours').textContent = hours
-      .toString()
-      .padStart(2, '0');
-    document.getElementById('minutes').textContent = minutes
-      .toString()
-      .padStart(2, '0');
-    document.getElementById('seconds').textContent = seconds
-      .toString()
-      .padStart(2, '0');
-  }
-  
-  setInterval(updateCountdown, 1000);
-  
   // Hiệu ứng mây trôi
   function animateClouds() {
     const clouds = document.querySelectorAll('.cloud');
